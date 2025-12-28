4cdc5b2de0a80ef4f243cdc00122c613cdb98897
ssk0206 <coccinelle.sasa@gmail.com>
2025-12-28 19:42:12 +0900

    Initialize project with basic structure and configuration files

---
+++ bin/tinymq.dart
@@ -0,0 +1,5 @@
+import 'package:tinymq/tinymq.dart' as tinymq;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tinymq.calculate()}!');
+}
---
+++ test/tinymq_test.dart
@@ -0,0 +1,8 @@
+import 'package:tinymq/tinymq.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
